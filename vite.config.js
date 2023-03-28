2a8f094c5e8b5e6896d789f8f95b693d204d3eaf
Antoine Weverbergh <weverant@me.com>
2023-03-28 15:27:55 +0200

    vite setup

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+process.env.BROWSER = 'firefox';
+
+const root = resolve(__dirname, 'src');
+
+export default defineConfig({
+    server: {
+        open: '/index.html',
+    },
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                wishapp: resolve(root, 'wishapp', 'index.html'),
+            },
+        },
+    },
+    root: 'src',
+});
