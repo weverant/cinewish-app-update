4307ef9e5704080d09b5755dbaf416470b8c4a8d
Antoine Weverbergh <weverant@me.com>
2023-04-04 03:07:57 +0200

    asset fix

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,6 +1,8 @@
+import imgUrl from '../assets/images/404.jpg';
+
 const ORIGINAL_POSTER_URL = 'https://image.tmdb.org/t/p/original';
 const POSTER_URL = (size) => `https://image.tmdb.org/t/p/w${size}`;
-const POSTER_404 = '../assets/images/404.jpg';
+const POSTER_404 = imgUrl;
 
 const MOVIE_URL = `/.netlify/functions/fetch-movies?id=`;
 const FEATURED_URL = `/.netlify/functions/fetch-featured-movies`;
